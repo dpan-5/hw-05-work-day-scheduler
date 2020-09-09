47e5ce83bd6746449b74e4e925a425315a9d6f5f
David Pan <dpan5425@gmail.com>
2020-09-09 15:44:10 -0400

    added loop to dynamically update HTML to display time blocks using bootstrap grid elements

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,35 @@
 $(document).ready(function() {
-
+    // Sets text content of HTML element with id of currentDay to today's date using moment.js
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+    // Array for time - used to dynamically create time blocks on the page
+    const timeArr = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
 
+    let mainDisplay = $("#displayContent");
+
+    // Loops through timeArr to dynamically create display
+    timeArr.forEach(element => {
+        // div row containing each time block
+        var newRow = $("<div>").addClass("row");
+
+        // div for column with time
+        var col1 = $("<div>").addClass("col-sm-2 time-block");
+        col1.text(element);
+
+
+
+        // var col2 = $("<textarea>").addClass("col-sm-9 time-block");
+        // var col3 = $("<div>").addClass("col-sm-1 time-block");
+
+
+        newRow.append(col1);
+        mainDisplay.append(newRow);
+
+
+
+
+
+    });
 
 });
+
