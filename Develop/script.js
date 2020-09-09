0f01f79eca09547f9c8dc185f8ab2491ce1b52af
David Pan <dpan5425@gmail.com>
2020-09-09 17:52:55 -0400

    working through time logic

--- Develop/script.js
+++ Develop/script.js
@@ -3,37 +3,54 @@ $(document).ready(function() {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
     // Array for time - used to dynamically create time blocks on the page
-    const timeArr = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+    const timeArr = ["9", "10", "11", "12", "1", "2", "3", "4", "5", "6" , "7"];
 
+    // Assigning container div to variable, to grab and append to at the end of loop below
     let mainDisplay = $("#displayContent");
 
+    var newBreakPoint;
+
     // Loops through timeArr to dynamically create display
-    timeArr.forEach(element => {
-        // create div row containing each time block
+    timeArr.forEach((element, index) => {
+        // ROW: create div row containing each time block
         var newRow = $("<div>").addClass("row");
 
-        // create column div for first column with time (2 blocks)
+        // COLUMN 1: create column div for first column with time (2 blocks)
         var col1 = $("<div>").addClass("col-sm-2 time-block hour");
-        col1.text(element);
+            // sets text to AM or PM based on array element/time
+            if(element >= 9 && element < 12) {
+                col1.text(`${element}AM`);
+            }
+            else {
+                col1.text(`${element}PM`);
+            }
 
-        // create column div for second column with text area (9 blocks)
-        var col2 = $("<textarea>").addClass("col-sm-9 past row");
+        // COLUMN 2: create column div for second column with text area (9 blocks)
+        var col2 = $("<textarea>").addClass("col-sm-9 row");
 
-        // create column div for third column with save button (1 block)
-        var col3 = $("<button>").addClass("col-sm-1 saveBtn");
-        col3.attr("type", "submit");
+            // sets div class to display present and captures index at which the element is present, to be used in a later loop
+            if(element === moment().format("h")) {
+                col2.addClass("present");
+            }
+            if(moment().hour() > 12 && index > 6) {
+                col2.addClass("future");
+            }
 
+        // COLUMN 3: create column div for third column with save button (1 block)
+        var col3 = $("<button type='submit'>").addClass("col-sm-1 saveBtn");
+        col3.append($("<i class='fas fa-save'>"));
 
+        // appending newly created columns to newly created row, and then appending row to container div
         newRow.append(col1);
         newRow.append(col2);
         newRow.append(col3);
         mainDisplay.append(newRow);
-
-
-
-
-
     });
+
+    // for(var i = 0; i < newBreakPoint; i++) {
+    //     $("").append($("<textarea>").addClass("past"));
+    // }
+
 
 });
 
